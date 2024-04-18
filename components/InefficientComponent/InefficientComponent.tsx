2162007b1ab2710ddbb5d52a37762b36e35cc97a
Mariya George <mgkofficial001@gmail.com>
2024-04-18 22:22:35 +0530

    formatted the files

--- components/InefficientComponent/InefficientComponent.tsx
+++ components/InefficientComponent/InefficientComponent.tsx
@@ -1,28 +1,31 @@
-'use client';
+"use client";
 import { useState } from "react";
-import TextInput from "../TextInput/TextInput"
+import TextInput from "../TextInput/TextInput";
 
 const InefficientComponent = (): JSX.Element => {
-    const [value, setValue] = useState<string>("");
-    const [list, setList] = useState<string[]>([]);
+  const [value, setValue] = useState<string>("");
+  const [list, setList] = useState<string[]>([]);
 
-    const handleTextInputValueChange = (value: string): void => {
-        setValue(value);
-        const l: string[] = [];
-        for (let i = 0; i < 20000; i++) {
-            l.push(value);
-        }
-        setList(l);
+  const handleTextInputValueChange = (value: string): void => {
+    setValue(value);
+    const l: string[] = [];
+    for (let i = 0; i < 20000; i++) {
+      l.push(value);
     }
+    setList(l);
+  };
 
-    return (
-        <>
-            <TextInput value={value} handleTextInputValueChange={handleTextInputValueChange} />
-            {list.map((listItem, index) => {
-                return <div key={index}>{listItem}</div>
-            })}
-        </>
-    )
-}
+  return (
+    <>
+      <TextInput
+        value={value}
+        handleTextInputValueChange={handleTextInputValueChange}
+      />
+      {list.map((listItem, index) => {
+        return <div key={index}>{listItem}</div>;
+      })}
+    </>
+  );
+};
 
 export default InefficientComponent;
--- components/TextInput/TextInput.tsx
+++ components/TextInput/TextInput.tsx
@@ -1,12 +1,22 @@
-'use client';
+"use client";
 import styles from "./TextInput.module.scss";
 
 interface ITextInputProps {
-    value: string;
-    handleTextInputValueChange: (value: string) => void;
+  value: string;
+  handleTextInputValueChange: (value: string) => void;
 }
-const TextInput = ({ value, handleTextInputValueChange }: ITextInputProps): JSX.Element => {
-    return <input type="text" className={styles["text-input"]} value={value} onChange={(event) => handleTextInputValueChange(event.target.value)}></input>
-}
+const TextInput = ({
+  value,
+  handleTextInputValueChange,
+}: ITextInputProps): JSX.Element => {
+  return (
+    <input
+      type="text"
+      className={styles["text-input"]}
+      value={value}
+      onChange={(event) => handleTextInputValueChange(event.target.value)}
+    ></input>
+  );
+};
 
 export default TextInput;
